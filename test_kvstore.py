9a5167628b72a9ecf8f630cf16c74b8334a2058e
Paul Logston <paul.logston@gmail.com>
2019-06-24 12:57:02 -0500

    Working smoke test for kvstore

--- test_kvstore.py
+++ test_kvstore.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 from socket import socketpair
 
 from kvstore import KVServer, KVClient
@@ -6,11 +7,12 @@ from kvstore import KVServer, KVClient
 def test_kvstore_smoke():
     s1, s2 = socketpair()
 
-
-
-    KVServer(s1).run()
+    server = Process(target=lambda sock: KVServer(sock).run(), args=(s1,))
+    server.start()
 
     client = KVClient(s2)
     client.set('k1', 'v1')
     assert client.get('k1') == 'v1'
+
+    server.terminate()
 
