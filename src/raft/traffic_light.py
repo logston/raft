998dd21f2856cafdaebd39d6b552c369df006e83
Paul Logston <paul.logston@gmail.com>
2019-06-25 12:20:59 -0500

    More statey traffic light

--- src/raft/traffic_light.py
+++ src/raft/traffic_light.py
@@ -1,70 +1,105 @@
 import time
+import sys
+from queue import Queue
+from threading import Thread
+
 
 import keyboard
 
 
-def change_state():
-    out1, out2 = 'G', 'R'
+class Event:
+    pass
+
+
+class ButtonPress(Event):
+    pass
+
+
+class Timeout(Event):
+    def __init__(self, tick):
+        self.tick = tick
+
+
+class TrafficLight:
+    STATES = ['G', 'Y', 'R']
+
+    def __init__(self):
+        self.out1 = 0
+        self.out2 = 2
+
+    def change_state(self):
+        if self.out1 == 0 and self.out2 == 2:
+            self.out1 = 1
+
+        elif self.out1 == 1 and self.out2 == 2:
+            self.out1 = 2
+            self.out2 = 0
+
+        elif self.out1 == 2 and self.out2 == 0:
+            self.out1 = 2
+            self.out2 = 1
+
+        elif self.out1 == 2 and self.out2 == 1:
+            self.out1 = 0
+            self.out2 = 2
+
+    def __repr__(self):
+        return ' '.join((self.STATES[self.out1], self.STATES[self.out2]))
+
+
+def run_queue(queue, light):
+
+    event = None
+    ticks = 0
+    while True:
+
+        if event is None:
+            event = queue.get_nowait()
+
+        print(ticks, ': ', light)
+        ticks += 1
+        time.sleep(1)
+
+        if isinstance(event, ButtonPress):
+            if light.out1 == 1:
+                light.change_state()
+                queue.queue.clear()
+                queue.put(Timeout(ticks + 5))
+                event = None
+
+        elif isinstance(event, Timeout) and event.tick <= ticks:
+            if light.out1 in (0, 2):
+                light.change_state()
+                queue.put(Timeout(ticks + 5))
+                event = None
+
+            elif light.out1 == 1:
+                light.change_state()
+                queue.put(Timeout(ticks + 30))
+                event = None
+
+
+def event_listener(queue):
+    while True:
+        sys.stdin.readline()
+        print('here')
+        queue.put(ButtonPress())
+
+
+def main():
+    light = TrafficLight()
+    queue = Queue()
+
+    queue.put(Timeout(5))
+    thread = Thread(target=run_queue, args=(queue, light), daemon=True)
+    thread.start()
+    thread = Thread(target=event_listener, args=(queue,), daemon=True)
+    thread.start()
 
     while True:
-        if out1 == 'G' and out2 == 'R':
-            out1, out2 = 'Y', 'R'
-
-        elif out1 == 'Y' and out2 == 'R':
-            out1, out2 = 'R', 'G'
-
-        elif out1 == 'R' and out2 == 'G':
-            out1, out2 = 'R', 'Y'
-
-        elif out1 == 'R' and out2 == 'Y':
-            out1, out2 = 'G', 'R'
-
-        yield out1, out2
-
-
-def sleep(cycles):
-    for i in range(cycles):
-        time.sleep(0.5)
-        yield i
-
-
-def get_button():
-    pressed = keyboard.is_pressed('space')
-    if pressed:
-        print('Button was pressed')
-
-    return pressed
-
-
-def cycle():
-    state_machine = change_state()
-    button = False
-
-    while True:
-        out1, out2 = next(state_machine)
-        print(out1, out2)
-
-        if out1 == 'G' and out2 == 'R':
-            for i in sleep(6):
-                if not button:
-                    button = get_button()
-                if button and i > 2:
-                    button = False
-                    break
-
-        elif out1 == 'Y' and out2 == 'R':
-            for i in sleep(1):
-                pass
-
-        elif out1 == 'R' and out2 == 'G':
-            for i in sleep(2):
-                pass
-
-        elif out1 == 'R' and out2 == 'Y':
-            for i in sleep(1):
-                pass
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    cycle()
+    main()
 
