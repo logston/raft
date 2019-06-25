2120f57c0c097aa811710d6ffa4f5159160b8f94
Paul Logston <paul.logston@gmail.com>
2019-06-25 14:19:22 -0500

    Paul's changes for walk

--- src/raft/light.py
+++ src/raft/light.py
@@ -10,23 +10,32 @@ INITIAL = {
 
 G1 = lambda s: (s['out1'] == 'G' and
                 s['out2'] == 'R' and
-                ((s['clock'] < 30 and dict(s, clock=s['clock'] + 1)) or
-                 (s['clock'] == 30 and dict(s, clock=0, out1='Y'))))
+                (
+                    (s['clock'] < 30 and dict(s, clock=s['clock'] + 1, walk=False)) or
+                    (s['clock'] == 30 and dict(s, clock=0, out1='Y', walk=False)))
+               )
 
 Y1 = lambda s: (s['out1'] == 'Y' and
                 s['out2'] == 'R' and
-                ((s['clock'] < 5 and dict(s, clock=s['clock'] + 1)) or
-                 (s['clock'] == 5 and dict(s, clock=0, out1='R', out2='G'))))
+                (
+                    (s['clock'] < 5 and dict(s, clock=s['clock'] + 1, walk=False)) or
+                    (s['clock'] == 5 and dict(s, clock=0, out1='R', out2='G', walk=False)))
+               )
 
 G2 = lambda s: (s['out1'] == 'R' and
                 s['out2'] == 'G' and
-                ((s['clock'] < 60 and dict(s, clock=s['clock'] + 1)) or
-                 (s['clock'] == 60 and dict(s, clock=0, out2='Y'))))
+                (
+                    (s['clock'] >= 30 and s['walk'] and dict(s, clock=0, out2='Y', walk=False)) or
+                    (s['clock'] < 60 and dict(s, clock=s['clock'] + 1)) or
+                    (s['clock'] == 60 and dict(s, clock=0, out2='Y', walk=False)))
+               )
 
 Y2 = lambda s: (s['out1'] == 'R' and
                 s['out2'] == 'Y' and
-                ((s['clock'] < 5 and dict(s, clock=s['clock'] + 1)) or
-                 (s['clock'] == 5 and dict(s, clock=0, out1='G', out2='R'))))
+                (
+                    (s['clock'] < 5 and dict(s, clock=s['clock'] + 1, walk=False)) or
+                    (s['clock'] == 5 and dict(s, clock=0, out1='G', out2='R', walk=False)))
+               )
 
 TICK = lambda evt, s: evt == 'tick' and (G1(s) or Y1(s) or G2(s) or Y2(s))
 BUTTON = lambda evt, s: evt == 'button' and dict(s, walk=True)
@@ -42,7 +51,7 @@ def run():
 
     def run_timer():
         while True:
-            time.sleep(1)
+            time.sleep(0.2)
             event_queue.put('tick')
 
     def run_button():
